a061fd55fe0ef482993acfed3a726bb1d92f1640
Boris <z.zz30@aol.com>
2016-12-17 14:01:17 +0100

    gitlab-ci test

--- Makefile.sh
+++ Makefile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #simple ass auto compile based on that zdoom wiki thing
-rm -f ../microtank_git.pk3; zip -0r ../microtank_git.pk3 . 
+rm -f ../microtank_git.pk3; zip -8r ../microtank_git.pk3 . 
